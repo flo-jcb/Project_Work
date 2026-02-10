cc0ae04857840ae7dbc975b6e8fadf76473e38e4
flo-jcb <flo.jacob150@gmail.com>
2026-02-10 16:21:17 +0100

    updt

--- src/motor_control/main.cpp
+++ src/motor_control/main.cpp
@@ -1,16 +1,28 @@
-#include "modbus_interface.hpp"
+#include <ModbusClientPort.h>
+#include <ModbusClient.h>
+#include <ModbusServerPort.h>
 #include <iostream>
 
-int main() {
-    MotorController motor("/dev/ttyUSB0", 19200, 1);
+void main()
+{   Modbus::TcpSettings settings;
+    settings.host = "EM-347B";
+    settings.port = Modbus::STANDARD_TCP_PORT;
+    settings.timeout = 3000;
+    ModbusClientPort *port = Modbus::createClientPort(Modbus::TCP, &settings, true);
+    ModbusClient c1(1, port);
+    ModbusClient c2(2, port);
+    ModbusClient c3(3, port);
+    ModbusClient c4(4, port);
+    Modbus::StatusCode s1, s2, s3, s4;
+    uint16_t regs1[10], regs2[10], regs3[10],regs4[10];
+    while(1)
+    {
+        s1 = c1.readHoldingRegisters(0, 10, regs1);
+        s2 = c2.readHoldingRegisters(0, 10, regs2);
+        s3 = c3.readHoldingRegisters(0, 10, regs3);
+        s4 = c4.readHoldingRegisters(0, 10, regs4);
+        Modbus::msleep(1);
+    }
 
-    /*if (!motor.connect()) return -1;
-
-    motor.setSpeed(500);
-    motor.setPosition(1000);
-
-    int pos = motor.readPosition();
-    std::cout << "Position actuelle: " << pos << std::endl;*/
-
-    return 0;
 }
+
