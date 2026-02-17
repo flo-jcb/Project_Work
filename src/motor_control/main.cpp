16cdbd03ddf707a316748faafd48c8d62fb9217a
flo-jcb <flo.jacob150@gmail.com>
2026-02-17 10:59:01 +0100

    update

--- src/motor_control/main.cpp
+++ src/motor_control/main.cpp
@@ -5,37 +5,32 @@ int main()
 {
     Modbus::SerialSettings settings;
 
-    settings.portName = "/dev/AMA0";   // Pico vu comme port USB
+    settings.portName = "/dev/ttyAMA0";
     settings.baudRate = 19200;
     settings.parity = Modbus::EvenParity;
     settings.dataBits = 8;
-    settings.timeoutInterByte = 1000;
 
     ModbusClientPort* port =
         Modbus::createClientPort(Modbus::RTU, &settings, true);
 
-    ModbusClient motor(1, port);   // Slave ID = 1 (EM347)
+    if (!port)
+    {
+        std::cout << "Port creation failed" << std::endl;
+        return -1;
+    }
+
+    ModbusClient motor(1, port);
 
     uint16_t value;
 
-    // Activer le bus
-    //motor.writeSingleRegister(0, 1);   // 40001
-
-    // Régler la vitesse
-   // motor.writeSingleRegister(1, 500); // 40002
-
-    // Lire vitesse réelle (exemple registre 10)
-
-    Modbus::StatusCode status = motor.readHoldingRegisters(10, 1, &value);
+    Modbus::StatusCode status =
+        motor.readHoldingRegisters(11, 1, &value);
 
     if (Modbus::StatusIsGood(status))
-    {
-        std::cout << "Feedback : " << value << std::endl;
-    }
+        std::cout << "OK: " << value << std::endl;
     else
-    {
-        std::cout << "Error code: " << status << std::endl;
-    }
+        std::cout << "Error: " << status << std::endl;
+
 
 
     return 0;
