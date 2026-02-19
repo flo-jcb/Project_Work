a983011ae17f461d45e0822f66f147b694832684
flo-jcb <flo.jacob150@gmail.com>
2026-02-19 11:10:45 +0100

    updt

--- src/motor_control/main.cpp
+++ src/motor_control/main.cpp
@@ -18,50 +18,53 @@ int main()
         std::cout << "Port creation failed" << std::endl;
         return -1;
     }
-    else std::cout << "Port created" << std::endl;
-
 
     ModbusClient motor(1, port); // Slave ID 1
 
     uint16_t value;
 
-    Modbus::StatusCode status =
-        motor.readInputRegisters(3, 1, &value);
+    Modbus::StatusCode status;
 
-    if (Modbus::StatusIsGood(status))
-        std::cout << "Courrant actuel = " << value << std::endl;
-    else
-        std::cout << "Erreur lecture " << status << std::endl;
+    while(1){
+        // Lire registre 1 (courrant en dV)
+        status = motor.readInputRegisters(3, 1, &value);
 
-    // Ecrire registre 1 (vitesse)
-    status = motor.writeSingleRegister(1,100);
+        if (Modbus::StatusIsGood(status))
+            std::cout << "Courrant actuel = " << value << std::endl;
+        else
+            std::cout << "Erreur lecture " << status << std::endl;
 
-    if (Modbus::StatusIsGood(status))
-        std::cout << "Ecriture OK" << std::endl;
-    else
-        std::cout << "Erreur ecriture : " << status << std::endl;
-    
-    //Lire vitesse rentrée
-    status = motor.readHoldingRegisters(1, 1, &value);
-    if (Modbus::StatusIsGood(status))
-        std::cout << "Vitesse actuel = " << value << std::endl;
-    else
-        std::cout << "Erreur lecture " << status << std::endl;
+        // Ecrire registre 1 (vitesse)
+        status = motor.writeSingleRegister(1,100);
 
-    // Lire registre 7 (vitesse)
-    status = motor.readInputRegisters(7, 1, &value);
+        if (Modbus::StatusIsGood(status))
+            std::cout << "Ecriture OK, vitesse 100" << std::endl;
+        else
+            std::cout << "Erreur ecriture : " << status << std::endl;
+        
+        //Lire vitesse rentrée
+        status = motor.readHoldingRegisters(1, 1, &value);
+        if (Modbus::StatusIsGood(status))
+            std::cout << "Vitesse actuel = " << value << std::endl;
+        else
+            std::cout << "Erreur lecture " << status << std::endl;
 
-    if (Modbus::StatusIsGood(status))
-        std::cout << "Vitesse actuel = " << value << std::endl;
-    else
-        std::cout << "Erreur lecture " << status << std::endl;
+        // Lire registre 7 (vitesse)
+        status = motor.readInputRegisters(7, 1, &value);
 
-    // Lire registre 1 (Motor Current)
-    status = motor.readInputRegisters(1, 1, &value);
+        if (Modbus::StatusIsGood(status))
+            std::cout << "Vitesse actuel = " << value << std::endl;
+        else
+            std::cout << "Erreur lecture " << status << std::endl;
 
-    if (Modbus::StatusIsGood(status))
-        std::cout << "Vitesse actuel = " << value << std::endl;
-    else
-        std::cout << "Erreur lecture " << status << std::endl;
+        // Lire registre 1 (Motor frequency)
+        status = motor.readInputRegisters(4, 1, &value);
+
+        if (Modbus::StatusIsGood(status))
+            std::cout << "Fréquence actuel = " << value << std::endl;
+        else
+            std::cout << "Erreur lecture " << status << std::endl;
+    }
+
     return 0;
 }
