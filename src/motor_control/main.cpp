ffbd895cd72ce830c8b4f80eb559d7ab29495719
flo-jcb <flo.jacob150@gmail.com>
2026-04-02 09:59:21 +0200

    updt

--- src/motor_control/main.cpp
+++ src/motor_control/main.cpp
@@ -277,12 +277,12 @@ int main()
 
     // ----------------------------
     // 6️⃣ Choisir direction (1 = avant)
-    //motor.writeSingleRegister(3, 1);
+    motor.writeSingleRegister(3, 1);
     sleep(0.2);
 
     // ----------------------------
     // 7️⃣ Écrire vitesse pour test
-    //motor.writeSingleRegister(1, 400); // vitesse max (ajuster selon doc EM-347B)
+    motor.writeSingleRegister(1, 400); // vitesse max (ajuster selon doc EM-347B)
     sleep(0.5);
 
 
